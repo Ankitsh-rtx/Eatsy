ce46654ed89c48845cab6b007c03c7268399ee00
Ankit <ankitshaw1112@gmail.com>
2023-08-02 19:24:10 +0530

    Activity-addition, UI responsive

--- app/src/main/java/com/example/eatsy/MainActivity.kt
+++ app/src/main/java/com/example/eatsy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.eatsy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.get
+import androidx.fragment.app.Fragment
 import com.example.eatsy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,24 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            var fragment: Fragment? = null
+            when (it.itemId) {
+                R.id.homeFragment -> HomeFragment().also { fragment = it }
+                R.id.discoverFragment -> DiscoverFragment().also { fragment = it }
+                R.id.trackOrderFragment -> TrackOrderFragment().also { fragment = it }
+                R.id.profileFragment -> ProfileFragment().also { fragment = it }
+                else -> {}
+            }
+            fragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView, it) // Replace "fragmentContainer" with your actual fragment container ID
+                    .commit()
+            }
 
+            true
+        }
     }
+
+
 }
