55876e664952e07141a7e46c8a5a3799d708f6b0
Ankit <ankitshaw1112@gmail.com>
2023-09-07 16:42:55 +0530

    ui enhancement

--- app/src/main/java/com/example/eatsy/views/ProfileFragment.kt
+++ app/src/main/java/com/example/eatsy/views/ProfileFragment.kt
@@ -9,25 +9,30 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.eatsy.R
 import com.example.eatsy.databinding.ActivityMainBinding
+import com.example.eatsy.databinding.FragmentProfileBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class ProfileFragment : Fragment() {
-
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
+        binding = FragmentProfileBinding.inflate(layoutInflater)
         val navBar = activity!!.findViewById<BottomAppBar>(R.id.bottomAppBar)
         navBar.visibility = View.GONE
 
 //        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
 //        activity?.window?.statusBarColor = context?.let { ContextCompat.getColor(it, R.color.ash_white) }!!
+        binding.backBtn.setOnClickListener{
+            activity?.supportFragmentManager?.popBackStackImmediate()
+        }
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        return binding.root
     }
 
 
