825794e348021f2dd6666cb3357d9f1892b9abd9
Ankit <ankitshaw1112@gmail.com>
2023-09-09 01:26:17 +0530

    bottom navigation icon change

--- app/src/main/java/com/example/eatsy/views/CartFragment.kt
+++ app/src/main/java/com/example/eatsy/views/CartFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -85,9 +86,13 @@ class CartFragment : Fragment() {
             val dialog = BottomSheetDialog(requireContext())
             val bottomSheetDialog =
                 LayoutInflater.from(context).inflate(R.layout.bottom_address_dialog, null)
-
             dialog.setContentView(bottomSheetDialog)
             dialog.show()
+
+            val addAddress = activity?.findViewById<ConstraintLayout>(R.id.addAddress)
+            addAddress?.setOnClickListener {
+                //TODO: open dialog
+            }
         }
 
         val total:Long=totalPrice()
