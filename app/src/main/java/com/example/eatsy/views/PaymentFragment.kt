9cf4f8bb6c567755ad847a0715c535654b8df0a8
Ankit <ankitshaw1112@gmail.com>
2023-09-09 01:10:09 +0530

    bottom navigation icon change

--- app/src/main/java/com/example/eatsy/views/PaymentFragment.kt
+++ app/src/main/java/com/example/eatsy/views/PaymentFragment.kt
@@ -6,14 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eatsy.R
+import com.example.eatsy.databinding.FragmentPaymentBinding
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class PaymentFragment : Fragment() {
-
+    private lateinit var binding: FragmentPaymentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        binding = FragmentPaymentBinding.inflate(layoutInflater)
+        val navBar = activity!!.findViewById<BottomAppBar>(R.id.bottomAppBar)
+        navBar.visibility = View.GONE
+
+        binding.backStackBtn.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStackImmediate()
+        }
+        binding.googlepay.isChecked = true
+
+//        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+//            val selectedId = group.checkedRadioButtonId
+//        }
+
+
+        return binding.root
     }
 }
