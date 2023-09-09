01417c8661f9b69fdb94bbcd100d1134626ae623
Ankit <ankitshaw1112@gmail.com>
2023-09-10 01:44:15 +0530

    authentication layout link

--- app/src/main/java/com/example/eatsy/views/OtpFragment.kt
+++ app/src/main/java/com/example/eatsy/views/OtpFragment.kt
@@ -1,60 +1,40 @@
 package com.example.eatsy.views
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eatsy.R
+import com.example.eatsy.databinding.FragmentOtpBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OtpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OtpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentOtpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_otp, container, false)
+        binding = FragmentOtpBinding.inflate(layoutInflater)
+
+        val bundle = arguments
+        val number = bundle?.getString("phone")
+
+        binding.backBtnOtp.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStackImmediate()
+        }
+
+        binding.verifyNumTV.text = "Verify with OTP sent to $number"
+
+        binding.confirmOtp.setOnClickListener {
+            val intent = Intent(requireContext(),MainActivity::class.java )
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK )
+            intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            activity?.finish()
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OTPFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OtpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
