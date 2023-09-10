ebbbf84f71fa9a6237743243dec402790d9bbf9a
Ankit <ankitshaw1112@gmail.com>
2023-09-10 10:31:21 +0530

    otp login screen ui

--- app/src/main/java/com/example/eatsy/views/OtpFragment.kt
+++ app/src/main/java/com/example/eatsy/views/OtpFragment.kt
@@ -2,12 +2,17 @@ package com.example.eatsy.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.eatsy.R
 import com.example.eatsy.databinding.FragmentOtpBinding
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 
 class OtpFragment : Fragment() {
     private lateinit var binding: FragmentOtpBinding
@@ -25,6 +30,24 @@ class OtpFragment : Fragment() {
             requireActivity().supportFragmentManager.popBackStackImmediate()
         }
 
+        //GenericTextWatcher here works only for moving to next EditText when a number is entered
+        //first parameter is the current EditText and second parameter is next EditText
+        binding.ed1.addTextChangedListener(GenericTextWatcher(binding.ed1, binding.ed2))
+        binding.ed2.addTextChangedListener(GenericTextWatcher(binding.ed2, binding.ed3))
+        binding.ed3.addTextChangedListener(GenericTextWatcher(binding.ed3, binding.ed4))
+        binding.ed4.addTextChangedListener(GenericTextWatcher(binding.ed4, binding.ed5))
+        binding.ed5.addTextChangedListener(GenericTextWatcher(binding.ed5, binding.ed6))
+        binding.ed6.addTextChangedListener(GenericTextWatcher(binding.ed6, null))
+
+        //GenericKeyEvent here works for deleting the element and to switch back to previous EditText
+        //first parameter is the current EditText and second parameter is previous EditText
+        binding.ed1.setOnKeyListener(GenericKeyEvent(binding.ed1, null))
+        binding.ed2.setOnKeyListener(GenericKeyEvent(binding.ed2, binding.ed1))
+        binding.ed3.setOnKeyListener(GenericKeyEvent(binding.ed3, binding.ed2))
+        binding.ed4.setOnKeyListener(GenericKeyEvent(binding.ed4,binding.ed3))
+        binding.ed5.setOnKeyListener(GenericKeyEvent(binding.ed5,binding.ed4))
+        binding.ed6.setOnKeyListener(GenericKeyEvent(binding.ed6,binding.ed5))
+
         binding.verifyNumTV.text = "Verify with OTP sent to $number"
 
         binding.confirmOtp.setOnClickListener {
@@ -36,5 +59,53 @@ class OtpFragment : Fragment() {
         }
         return binding.root
     }
+
+}
+
+class GenericTextWatcher internal constructor(private val currentView: View, private val nextView: View?) :
+    TextWatcher {
+    override fun afterTextChanged(editable: Editable) { // TODO Auto-generated method stub
+        val text = editable.toString()
+        when (currentView.id) {
+            R.id.ed1 -> if (text.length == 1) nextView!!.requestFocus()
+            R.id.ed2 -> if (text.length == 1) nextView!!.requestFocus()
+            R.id.ed3 -> if (text.length == 1) nextView!!.requestFocus()
+            R.id.ed4 -> if (text.length == 1) nextView!!.requestFocus()
+            R.id.ed5 -> if (text.length == 1) nextView!!.requestFocus()
+
+
+            //You can use EditText4 same as above to hide the keyboard
+        }
+    }
+
+    override fun beforeTextChanged(
+        arg0: CharSequence,
+        arg1: Int,
+        arg2: Int,
+        arg3: Int
+    ) { // TODO Auto-generated method stub
+    }
+
+    override fun onTextChanged(
+        arg0: CharSequence,
+        arg1: Int,
+        arg2: Int,
+        arg3: Int
+    ) { // TODO Auto-generated method stub
+    }
+
+}
+
+class GenericKeyEvent internal constructor(private val currentView: EditText, private val previousView: EditText?) : View.OnKeyListener{
+    override fun onKey(p0: View?, keyCode: Int, event: KeyEvent?): Boolean {
+        if(event!!.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL && currentView.id != R.id.ed1 && currentView.text.isEmpty()) {
+            //If current is empty then previous EditText's number will also be deleted
+            previousView!!.text = null
+            previousView.requestFocus()
+            return true
+        }
+        return false
+    }
+
 
 }
