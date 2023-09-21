f227e171503f8e0299ee437ed25b4beb62d6fa25
Ankit <ankitshaw1112@gmail.com>
2023-09-22 01:28:58 +0530

    Search Screen added

--- app/src/main/java/com/example/eatsy/views/SearchFragment.kt
+++ app/src/main/java/com/example/eatsy/views/SearchFragment.kt
@@ -1,20 +1,48 @@
 package com.example.eatsy.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eatsy.R
+import com.example.eatsy.databinding.FragmentSearchBinding
+import com.example.eatsy.model.Item
+import com.example.eatsy.model.Restaurants
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SearchFragment : Fragment() {
-
+    private lateinit var binding:FragmentSearchBinding
+    private lateinit var db: FirebaseFirestore
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        db = FirebaseFirestore.getInstance()
+
+        binding.backSearchBtn.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStackImmediate()
+        }
+
+        binding.searchEdBtn.setOnClickListener {
+            Log.d("SearchFragment", "clicked")
+            val queryString = binding.searchEdittext.text?.trim().toString()
+            db.collection("Items").whereEqualTo("name",queryString).get()
+                .addOnSuccessListener {
+                for(doc in it.documents){
+                    val res = doc.toObject(Item::class.java)
+                    Log.d("SearchFragment:","doc ${res?.name}")
+                }
+            }.addOnFailureListener {
+                Log.d("SearchFragment","failed due to $it")
+            }
+        }
+
+
+        return binding.root
     }
 
 }
