7c15c31e81fa65d55acf685adfbe11c6dad606f4
Gourav Agarwal <aggourav5015@gmail.com>
2023-10-11 13:22:36 +0530

    click

--- app/src/main/java/com/example/eatsy/views/SearchFragment.kt
+++ app/src/main/java/com/example/eatsy/views/SearchFragment.kt
@@ -1,29 +1,22 @@
 package com.example.eatsy.views
 
 import android.os.Bundle
-import android.provider.ContactsContract.Data
-import android.provider.UserDictionary.Words.LOCALE
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.eatsy.DataSource
 import com.example.eatsy.R
 import com.example.eatsy.adapter.SearchAdapter
-import com.example.eatsy.adapter.SearchAdapterChildItems
+import com.example.eatsy.adapter.SearchRestaurantAdaptor
 import com.example.eatsy.databinding.FragmentSearchBinding
 import com.example.eatsy.model.Item
 import com.example.eatsy.model.Restaurants
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.LocalCacheSettings
 import java.util.Locale
-import java.util.Objects
-import kotlin.math.log
 
 class SearchFragment : Fragment() {
     private lateinit var binding:FragmentSearchBinding
@@ -44,11 +37,16 @@ class SearchFragment : Fragment() {
         }
 
         binding.searchResultRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+        binding.searchResultRecyclerview1.layoutManager = LinearLayoutManager(requireContext())
 
         binding.searchEdBtn.setOnClickListener {
 //            Log.d("SearchFragment", "clicked")
             DataSource.itemSearchList.clear()
+
             val queryString = binding.searchEdittext.text?.trim().toString()
+            var restaurantslist= DataSource.restaurants.filter {
+                it.name.contains(queryString)
+            }
             binding.searchResultTv.text = "Showing results for '$queryString'"
             val items:MutableList<Item> = mutableListOf()
             db.collection("Items").whereLessThanOrEqualTo("name",queryString).get()
@@ -94,6 +92,9 @@ class SearchFragment : Fragment() {
                         }
                     })
                     binding.searchResultRecyclerview.adapter = adapter
+
+                    val adapterRsetaurant = SearchRestaurantAdaptor(requireContext(),restaurantslist)
+                    binding.searchResultRecyclerview1.adapter=adapterRsetaurant
 
 
             }.addOnFailureListener {
